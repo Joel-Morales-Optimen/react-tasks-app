9588d4b0ac56de5a2d99eb37b0edab2e94f0b922
Joel-Morales-Optimen <joel.morales@optimen.com.mx>
2026-02-05 17:15:03 -0600

    Yacasi

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,17 +1,16 @@
-function TaskCard({ task }) {
+import TaskContext from "../context/TaskContext";
 
-  function eliminarTarea(){
-    
-  }
-
+function TaskCard({ task, deleteTask }) {
   return (
-    <div>
-      <h1>{task.title}</h1>
-      <p>{task.description}</p>
-      <button>
-        Eliminar Tarea
-      </button>
-    </div>
+    <TaskContext>
+      <div>
+        <h1>{task.title}</h1>
+        <p>{task.description}</p>
+        <button onClick={() => deleteTask(task.id)}>
+          Eliminar Tarea
+        </button>
+      </div>
+    </TaskContext>
   );
 }
 
---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,10 @@
+function TaskContext() {
+  return (
+    <>
+    <h1>Componente Context</h1>
+    {props.children}
+    </>
+  )
+}
+
+export default TaskContext
