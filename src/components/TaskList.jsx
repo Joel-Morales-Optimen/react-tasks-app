3a2062c62d86e365eeb54d4ba02f9afc8933bfb2
Joel-Morales-Optimen <joel.morales@optimen.com.mx>
2026-02-06 10:03:40 -0600

    Listo

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,11 +5,15 @@ function TaskList() {
   const { tasks } = useContext(TaskContext);
 
   if (tasks.length == 0) {
-    return <h1>No hay tareas aún.</h1>;
+    return (
+      <h1 className="text-white text-4xl font-bold text-center">
+        No hay tareas aún.
+      </h1>
+    );
   }
 
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-2 p-4 rounded-md">
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task} />
       ))}
